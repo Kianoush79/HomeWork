38a14a08a1a30653985e008500222e6955b079c8
Kianoush79 <kiakam79@gmail.com>
2021-08-15 17:51:24 -0400

    second git commit with all file

---
+++ src/git/gitclass02.java
@@ -0,0 +1,16 @@
+package git;
+
+public class gitclass02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	 System.out.println("hello");
+
+	
+	
+	
+	
+	}
+
+}
