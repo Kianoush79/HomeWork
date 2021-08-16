515d634529fa9de14e56356f8b5cc6637fb9b3ce
Kianoush79 <kiakam79@gmail.com>
2021-08-16 17:57:27 -0400

    from1 to 100

--- src/git/Homework.java
+++ src/git/Homework.java
@@ -5,7 +5,7 @@ public class Homework {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
- for (int a=1; a<=10; a++) {
+ for (int a=1; a<=100; a++) {
 	 System.out.println(a);
  }
 
