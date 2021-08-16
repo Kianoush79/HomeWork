ea1bdc495abc01761cc785ad2d8cfb691ad477ca
Kianoush79 <kiakam79@gmail.com>
2021-08-16 17:48:48 -0400

    homeworks

---
+++ src/git/Homework.java
@@ -0,0 +1,22 @@
+package git;
+
+public class Homework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+ for (int a=1; a<=10; a++) {
+	 System.out.println(a);
+ }
+
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
